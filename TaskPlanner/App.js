d8a34104bf35958233fffe417f742f8a38fe2b06
rahulmishra <rahulmishra.hello@gmail.com>
2018-02-09 10:58:06 +0530

    Interim commit

--- TaskPlanner/App.js
+++ TaskPlanner/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Animated, Text, View, Image } from 'react-native';
 
-export default class App extends React.Component {
-  render() {
+export default class LoginScreen extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {isShowingText: true};
+
+  setInterval(() => {
+    this.setState(previousState => {
+      return {isShowingText: !previousState.isShowingText};
+    });
+  }, 1000);
+}
+
+render() {
+    let display = this.state.isShowingText ? this.props.text : ' ';
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Text>{display}</Text>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
